a0e74fe5af67d1bde3e0f805886fff8bbaf8346a
Navneet Singh <navneetsinghlovely07@gmail.com>
2022-11-23 14:46:16 +0530

    Update long_subseq_ap.py

--- practise code/Basic python/long_subseq_ap.py
+++ practise code/Basic python/long_subseq_ap.py
@@ -1,16 +1,52 @@
 ###using classes 
-n=[1,2,3,4,5,6]
-B=[{} for _ in range(len(n))]
-max_B=1
+# n=[1,2,3,4,5,6]
+# B=[{} for _ in n]
+
+# def subseq(n):
+#     max_B=0
+#     if len(n)<2:
+#         return len(n)
+#     for i in range(len(n)):
+#         for j in range(0,i):
+#             cd=n[i]-n[j]
+#             B[i][cd]= max(B[i].get(cd,1),B[j].get(cd,1)+1)
+#             max_B= max(max_B,B[i][cd])
+#     return max_B
+# print(subseq(n))
+
+###using classes 
+# n=[1,2,3,4,5,6]
+# B=[{} for _ in range(len(n))]
+# print(B)
+
+# def subseq(n):
+#     max_B=0
+#     if len(n)<2:
+#         return n
+#     for i in range(len(n)):
+#         B[i][0]=1
+#         for j in range(0,i):
+#             cd=n[i]-n[j]
+#             if cd not in B[j]:
+#                 B[i][cd]=2
+#             else:
+#                 B[i][cd]=B[j][cd]+1
+#         max_B= max(max_B,max(B[i].values()))
+#     return max_B
+# print(subseq(n))
+
+n=input()
+n=n.split()
+B=[{} for _ in n]
+
 def subseq(n):
-    B[i][0]=1
+    max_B=0
     if len(n)<2:
-        return n
+        return len(n)
     for i in range(len(n)):
         for j in range(0,i):
             cd=n[i]-n[j]
-            if cd not in B[j]:
-                
+            B[i][cd]= max(B[i].get(cd,1),B[j].get(cd,1)+1)
             max_B= max(max_B,B[i][cd])
     return max_B
 print(subseq(n))
