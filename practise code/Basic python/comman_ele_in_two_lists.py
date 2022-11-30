710f4ac9ee10fe947a440830f4e9a45975335ad5
Navneet Singh <navneetsinghlovely07@gmail.com>
2022-11-30 17:09:20 +0530

    Create comman_ele_in_two_lists.py

---
+++ practise code/Basic python/comman_ele_in_two_lists.py
@@ -0,0 +1,46 @@
+
+
+num_in_list1= int(input("enter the number of elements in the list 1:  "))
+num_in_list2= int(input("enter the number of elements in the list 2:  "))
+list1=[]
+list2=[]
+for i in range(num_in_list1):
+    lst=int(input("enter the elements o fthe list 1:  "))
+    list1.append(lst)
+
+for i in range(num_in_list2):
+    lst=int(input("enter the elements o fthe list 2:  "))
+    list2.append(lst)
+
+
+
+#basic logic with O(n*m) time complexity 
+# com_cont=0
+# for i in range(len(list1)):
+#     for j in range(len(list2)):
+#         if list1[i]==list2[j]:
+#             print("common elements detected")
+#             print(list1[i])
+#             com_cont+=1
+# print("number of comman elements: ", com_cont)        
+######## print("No common element found in list 1 and list 2")
+
+#common elements in two lists in O(N) time and O(M) space IF n>m
+
+lis_dic={}
+for i in list2:
+    lis_dic[i]= 1
+print("Dict: {}".format(lis_dic))
+
+
+cnt=0
+for j in list1:
+    if lis_dic.get(j)!= None:
+        print(j)
+        cnt += 1
+print("Number of comman elements: ", cnt)
+
+
+
+
+
