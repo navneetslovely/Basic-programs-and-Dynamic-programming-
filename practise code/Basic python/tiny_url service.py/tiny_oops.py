a952b96299dc069e51dd4e060148871185d1d734
Navneet Singh <navneetsinghlovely07@gmail.com>
2022-12-01 18:27:48 +0530

    Update tiny_oops.py

--- practise code/Basic python/tiny_url service.py/tiny_oops.py
+++ practise code/Basic python/tiny_url service.py/tiny_oops.py
@@ -7,7 +7,7 @@ class ShortURL:
 
 
     def __init__(self):
-        self.d= dict()
+        self.url_dict= dict()
 
     def getShortUrl(self, longUrl):
         rnum= random.randint(6,10)
@@ -17,3 +17,20 @@ class ShortURL:
         shortURL =''
         for i in range(rnum):
             shortURL=''.join([random.choice(char),shortURL])
+        print(shortURL)
+
+        if shortURL in self.url_dict:
+            return getShortUrl(longUrl)
+        else:
+            self.url_dict[shortURL]= longUrl
+
+        short_link="www.google.com/".join(shortURL)
+        return short_link
+
+    def getLongUrl(self,shortURL):
+        long_link=shortURL[16:]
+
+        if long_link in self.url_dict:
+            return self.url_dict[long_link]
+        else:
+            return  None
