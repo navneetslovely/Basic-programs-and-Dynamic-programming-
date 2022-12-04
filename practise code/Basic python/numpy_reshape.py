554d004cd6688cb4b750ce7b937dce6bc7732656
Navneet Singh <navneetsinghlovely07@gmail.com>
2022-12-04 19:46:42 +0530

    Update numpy_reshape.py

--- practise code/Basic python/numpy_reshape.py
+++ practise code/Basic python/numpy_reshape.py
@@ -1,10 +1,33 @@
-# import numpy as np
+'''
+You are given a space separated list of nine integers. Your task is to convert this list into a 3X3 NumPy array.
 
-# given_list=input()
-given_list="1 2 3 4 5 6"
+Input Format
+
+A single line of input containing 9 space separated integers.
+
+Output Format
+
+Print the 3X3 NumPy array.
+
+Sample Input
+
+1 2 3 4 5 6 7 8 9
+Sample Output
+
+[[1 2 3]
+ [4 5 6]
+ [7 8 9]]'''
+
+
+
+import numpy as np
+
+lst=input("Enter the number ")
+lst= lst.split(" ")
 int_lst=[]
-given_list=given_list.split()
-for i in given_list:
-    
-    int_lst.append(i)
-print(int_lst)
+for i in range(len(lst)):
+    n=int(lst[i])
+    int_lst.append(n)
+glst=np.array(int_lst)
+
+print(np.reshape(glst,(3,3)))
