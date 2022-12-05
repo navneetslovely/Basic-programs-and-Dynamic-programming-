f30fb9bd4d538777066b48369851a76f46b2b5dd
Navneet Singh <navneetsinghlovely07@gmail.com>
2022-12-05 20:23:07 +0530

    Update square_witout_joblib.py

---
+++ practise code/Basic python/square_witout_joblib.py
@@ -0,0 +1,25 @@
+import time
+from math import sqrt
+
+
+
+
+def f(i):
+    num=10000000
+    pro=1
+    for j in range(num):
+        for k in range(j):
+            pro*=k
+    
+    return sqrt (i**2)
+
+
+rng= 10
+start_time=time.time()
+
+for i in range(rng):
+    f(i)
+
+
+end_time=time.time()
+print(end_time-start_time)
